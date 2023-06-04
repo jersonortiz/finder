e4863d6fa482ff31ac8ed722e9172f5133d9a61e
finderappdeploy <usuarioprovisional333@gmail.com>
2023-06-04 10:21:00 -0500

    correccion

--- src/main/webapp/dist/js/config.js
+++
@@ -1,4 +0,0 @@
-const prox = 'https://cors-anywhere.herokuapp.com/';
-//const serverurl = 'https://cors-anywhere.herokuapp.com/http://3.145.32.144:8080/';
-//const serverurl = 'http://ingsistemasufps.co/';
-const serverurl = 'http://3.145.32.144:8080/';
